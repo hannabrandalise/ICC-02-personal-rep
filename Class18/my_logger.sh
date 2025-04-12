222498f0444a29d964978ce6f4a40ddbe958aff2
hanna <hannabrandalise@gmail.com>
2025-04-12 14:16:21 +0000

    delete double file

--- Class18/my_logger.sh
+++ Class18/my_logger.sh
@@ -8,3 +8,13 @@ DATE=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Append the log entry to the file
 echo "[$DATE] User: $USER - Log entry: Script executed successfully " >> "$LOG_FILE"
+#!/bin/bash
+
+# Create a log file
+LOG_FILE="user_activity.log"
+
+# Generate the current date and time using the date command in the format
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Append the log entry to the file
+echo "[$DATE] User: $USER - Log entry: Script executed successfully " >> "$LOG_FILE"
